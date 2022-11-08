c394082df6844ef34bc89a81ad885472adfc72b3
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-11-08 16:00:00 +0100

    encoding: add G_GNUC_INTERNAL

--- tepl/tepl-encoding.h
+++ tepl/tepl-encoding.h
@@ -14,31 +14,44 @@ G_BEGIN_DECLS
 
 typedef struct _TeplEncoding TeplEncoding;
 
+G_GNUC_INTERNAL
 GType			tepl_encoding_get_type			(void) G_GNUC_CONST;
 
+G_GNUC_INTERNAL
 TeplEncoding *		tepl_encoding_new			(const gchar *charset);
 
+G_GNUC_INTERNAL
 TeplEncoding *		tepl_encoding_new_utf8			(void);
 
+G_GNUC_INTERNAL
 TeplEncoding *		tepl_encoding_new_from_locale		(void);
 
+G_GNUC_INTERNAL
 TeplEncoding *		tepl_encoding_copy			(const TeplEncoding *enc);
 
+G_GNUC_INTERNAL
 void			tepl_encoding_free			(TeplEncoding *enc);
 
+G_GNUC_INTERNAL
 const gchar *		tepl_encoding_get_charset		(const TeplEncoding *enc);
 
+G_GNUC_INTERNAL
 const gchar *		tepl_encoding_get_name			(const TeplEncoding *enc);
 
+G_GNUC_INTERNAL
 gchar *			tepl_encoding_to_string			(const TeplEncoding *enc);
 
+G_GNUC_INTERNAL
 gboolean		tepl_encoding_is_utf8			(const TeplEncoding *enc);
 
+G_GNUC_INTERNAL
 gboolean		tepl_encoding_equals			(const TeplEncoding *enc1,
 								 const TeplEncoding *enc2);
 
+G_GNUC_INTERNAL
 GSList *		tepl_encoding_get_all			(void);
 
+G_GNUC_INTERNAL
 GSList *		tepl_encoding_get_default_candidates	(void);
 
 G_END_DECLS
