e68633ca0db166325f9b91840bfbbbd78c5e113c
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-11-08 16:00:00 +0100

    MenuStackSwitcher: change license headers

--- tepl/tepl-menu-stack-switcher.h
+++ tepl/tepl-menu-stack-switcher.h
@@ -1,21 +1,6 @@
-/*
- * This file is part of gedit
- *
- * Copyright (C) 2014 - Steve Frécinaux
- * Copyright (C) 2022 - Sébastien Wilmet
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
+/* SPDX-FileCopyrightText: 2014 - Steve Frécinaux
+ * SPDX-FileCopyrightText: 2022 - Sébastien Wilmet <swilmet@gnome.org>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
 #ifndef TEPL_MENU_STACK_SWITCHER_H
