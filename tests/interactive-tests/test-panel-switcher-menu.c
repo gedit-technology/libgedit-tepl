0746c3909b4a6910760fa2934dc22f25a519717b
Sébastien Wilmet <swilmet@informatique-libre.be>
2023-07-11 16:00:00 +0200

    PanelSwitcherMenu test: take and modify the test for StackSwitcherMenu

--- tests/interactive-tests/test-panel-switcher-menu.c
+++ tests/interactive-tests/test-panel-switcher-menu.c
@@ -5,28 +5,27 @@
 #include <tepl/tepl.h>
 #include <stdlib.h>
 
-static TeplStack *
-create_stack (void)
+static TeplPanelContainer *
+create_panel (void)
 {
-	TeplStack *stack;
+	TeplPanelContainer *panel;
 	GtkWidget *label;
-	TeplStackItem *item;
+	TeplPanelItem *item;
 
-	stack = tepl_stack_new ();
+	panel = tepl_panel_container_new ();
 
 	label = gtk_label_new ("Widget 1");
-	item = tepl_stack_item_new (label, "name1", "Title 1", NULL);
-	tepl_stack_add_item (stack, item);
-	tepl_stack_set_visible_item (stack, item);
+	gtk_widget_show (label);
+	item = tepl_panel_add (TEPL_PANEL (panel), label, "name1", "Title 1", NULL);
 	g_object_unref (item);
 
 	label = gtk_label_new ("Widget 2");
-	item = tepl_stack_item_new (label, "name2", "Title 2", NULL);
-	tepl_stack_add_item (stack, item);
+	gtk_widget_show (label);
+	item = tepl_panel_add (TEPL_PANEL (panel), label, "name2", "Title 2", NULL);
 	g_object_unref (item);
 
-	gtk_widget_show (GTK_WIDGET (stack));
-	return stack;
+	gtk_widget_show (GTK_WIDGET (panel));
+	return panel;
 }
 
 int
@@ -34,9 +33,9 @@ main (int    argc,
       char **argv)
 {
 	GtkWidget *window;
-	TeplStack *stack;
+	TeplPanelContainer *panel;
 	GtkHeaderBar *headerbar;
-	TeplStackSwitcherMenu *stack_switcher_menu;
+	TeplPanelSwitcherMenu *switcher_menu;
 
 	tepl_init ();
 	gtk_init (&argc, &argv);
@@ -44,14 +43,14 @@ main (int    argc,
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_default_size (GTK_WINDOW (window), 600, 400);
 
-	stack = create_stack ();
-	gtk_container_add (GTK_CONTAINER (window), GTK_WIDGET (stack));
+	panel = create_panel ();
+	gtk_container_add (GTK_CONTAINER (window), GTK_WIDGET (panel));
 
 	headerbar = GTK_HEADER_BAR (gtk_header_bar_new ());
 	gtk_header_bar_set_show_close_button (headerbar, TRUE);
-	gtk_header_bar_set_title (headerbar, "TeplStackSwitcherMenu Test");
-	stack_switcher_menu = tepl_stack_switcher_menu_new (stack);
-	gtk_header_bar_pack_start (headerbar, GTK_WIDGET (stack_switcher_menu));
+	gtk_header_bar_set_title (headerbar, "TeplPanelSwitcherMenu Test");
+	switcher_menu = tepl_panel_switcher_menu_new (panel);
+	gtk_header_bar_pack_start (headerbar, GTK_WIDGET (switcher_menu));
 	gtk_widget_show_all (GTK_WIDGET (headerbar));
 	gtk_window_set_titlebar (GTK_WINDOW (window), GTK_WIDGET (headerbar));
 
