0bcfcfb7bb647d3eb448f9aeb887aa1bfe5f8854
Sébastien Wilmet <swilmet@informatique-libre.be>
2023-07-11 16:00:00 +0200

    Remove TeplStack* classes
    Superseded by TeplPanel* classes and interfaces.

--- tepl/tepl.h
+++ tepl/tepl.h
@@ -50,9 +50,6 @@
 #include <tepl/tepl-settings.h>
 #include <tepl/tepl-signal-group.h>
 #include <tepl/tepl-space-drawer-prefs.h>
-#include <tepl/tepl-stack.h>
-#include <tepl/tepl-stack-item.h>
-#include <tepl/tepl-stack-switcher-menu.h>
 #include <tepl/tepl-statusbar.h>
 #include <tepl/tepl-style-scheme-chooser-widget.h>
 #include <tepl/tepl-tab.h>
