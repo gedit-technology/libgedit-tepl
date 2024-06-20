df4f9f034dc3112f4dd2ea9cb1d2279010e277d0
Sébastien Wilmet <swilmet@mailfence.com>
2024-06-20 13:12:06 +0200

    PanelItem: unit-tests for compare() functions

--- tests/unit-tests/test-panel-item.c
+++ tests/unit-tests/test-panel-item.c
@@ -24,6 +24,47 @@ test_basics (void)
 	g_object_unref (item);
 }
 
+static void
+test_compare (void)
+{
+	GtkWidget *label1;
+	GtkWidget *label2;
+	TeplPanelItem *item1;
+	TeplPanelItem *item2;
+
+	label1 = gtk_label_new (NULL);
+	label2 = gtk_label_new (NULL);
+	g_object_ref_sink (label1);
+	g_object_ref_sink (label2);
+
+	/* All posititons at 0 */
+	item1 = tepl_panel_item_new (label1, "name1", "A", NULL, 0);
+	item2 = tepl_panel_item_new (label2, "name2", "B", NULL, 0);
+	g_assert_cmpint (tepl_panel_item_compare_by_title (item1, item2), <, 0);
+	g_assert_cmpint (tepl_panel_item_compare (item1, item2), <, 0);
+	g_assert_cmpint (tepl_panel_item_compare_by_title (item2, item1), >, 0);
+	g_assert_cmpint (tepl_panel_item_compare (item2, item1), >, 0);
+	g_assert_cmpint (tepl_panel_item_compare_by_title (item1, item1), ==, 0);
+	g_assert_cmpint (tepl_panel_item_compare (item1, item1), ==, 0);
+	g_object_unref (item1);
+	g_object_unref (item2);
+
+	/* With different positions */
+	item1 = tepl_panel_item_new (label1, "name1", "A", NULL, 1);
+	item2 = tepl_panel_item_new (label2, "name2", "B", NULL, 0);
+	g_assert_cmpint (tepl_panel_item_compare_by_title (item1, item2), <, 0);
+	g_assert_cmpint (tepl_panel_item_compare (item1, item2), >, 0);
+	g_assert_cmpint (tepl_panel_item_compare_by_title (item2, item1), >, 0);
+	g_assert_cmpint (tepl_panel_item_compare (item2, item1), <, 0);
+	g_assert_cmpint (tepl_panel_item_compare_by_title (item1, item1), ==, 0);
+	g_assert_cmpint (tepl_panel_item_compare (item1, item1), ==, 0);
+	g_object_unref (item1);
+	g_object_unref (item2);
+
+	g_object_unref (label1);
+	g_object_unref (label2);
+}
+
 int
 main (int    argc,
       char **argv)
@@ -31,6 +72,7 @@ main (int    argc,
 	gtk_test_init (&argc, &argv);
 
 	g_test_add_func ("/PanelItem/basics", test_basics);
+	g_test_add_func ("/PanelItem/compare", test_compare);
 
 	return g_test_run ();
 }
